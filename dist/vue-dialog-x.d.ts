6eb740e54411c295b9d89f42aa2a618b0a759617
a62527776a <a13868300128a@126.com>
2019-08-01 20:44:27 +0800

    11

---
+++ dist/vue-dialog-x.d.ts
@@ -0,0 +1,23 @@
+import _Vue from 'vue'
+
+declare module "*.vue" {
+  import Vue from 'vue'
+  export default Vue
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $dialog: string
+  }
+}
+
+declare class VueDialogX {
+  constructor(vue: _Vue, globalOptions: any)
+  confirm (): void
+  alert (): void
+  prompt (): void
+  actions (): void
+  dialog (): void
+}
+
+declare function install(Vue: typeof _Vue): void;
